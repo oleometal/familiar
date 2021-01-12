79abc0813aa9998c0c28b03ec52a1546320dce8d
irra <oleometal@gmail.com>
2021-01-11 21:29:49 -0600

    2129-11-01-21

--- publica/app.js
+++ publica/app.js
@@ -10,13 +10,77 @@ if('serviceWorker' in navigator) {
 	window.addEventListener('load',() =>{
 		navigator.serviceWorker.register('./internuncio.js')
 		.then(registro =>{
-			console.log('Internuncio esta registrado con alcance: ',registro.scope);
+			console.log('[app] Internuncio esta registrado con alcance: ',registro.scope);
+			reg.pushManager.getSubscription().then(function(sub) {
+				if (sub === null) {
+				  // Update UI to ask user to register for Push
+				  console.log('Not subscribed to push service!');
+				} else {
+				  // We have a subscription, update the database
+				  console.log('Subscription object: ', sub);
+				}
+			  });
+		  
 		})
 			.catch(err =>{
-				console.error('Registro falló: ',err);
+				console.error('[app] Registro falló: ',err);
 			});
 	});
 }
+/**-----------------------------------------------
+ * 			Notificación de Inserción
+ */
+/**	Pedir permiso */
+Notification.requestPermission(function(status) {
+    console.log('[app] Estado del Permiso de Notificaión:', status);
+});
+/**Mostrar una notificación */
+function displayNotification() {
+	if (Notification.permission == 'granted') {
+	  navigator.serviceWorker.getRegistration().then(function(reg) {
+		var options = {
+		  body: 'Here is a notification body!',
+		  icon: 'images/example.png',
+		  vibrate: [100, 50, 100],
+		  data: {
+			dateOfArrival: Date.now(),
+			primaryKey: 1
+		  },
+		  actions: [
+			{action: 'explore', title: 'Explore this new world',
+			  icon: 'images/checkmark.png'},
+			{action: 'close', title: 'Close notification',
+			  icon: 'images/xmark.png'},
+		  ]
+		};
+		reg.showNotification('Hello world!', options);
+	  });
+	}
+  }
+/**subscribir usuario */
+function subscribeUser() {
+	if ('serviceWorker' in navigator) {
+	  navigator.serviceWorker.ready.then(function(reg) {
+  
+		reg.pushManager.subscribe({
+		  userVisibleOnly: true
+		}).then(function(sub) {
+		  console.log('Endpoint URL: ', sub.endpoint);
+		}).catch(function(e) {
+		  if (Notification.permission === 'denied') {
+			console.warn('Permission for notifications was denied');
+		  } else {
+			console.error('Unable to subscribe to push', e);
+		  }
+		});
+	  })
+	}
+  }
+  
+  
+/**----------------------------------------------
+ * 				Fetch
+ */
 /**haciendo petición */
 function irABuscar(params) {
 	
--- publica/internuncio.js
+++ publica/internuncio.js
@@ -14,3 +14,49 @@ self.skipWaiting();
 self.addEventListener('fetch',e =>{
     console.log('[Nuncio] Obteniendo: ',e.request.url);
 });
+/**----------------------------------------------
+ *      Notificaciones
+ */
+/**escuchando evento  */
+self.addEventListener('notificationclose', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+  
+    console.log('Closed notification: ' + primaryKey);
+  });
+  /**evento click */
+  self.addEventListener('notificationclick', function(e) {
+    var notification = e.notification;
+    var primaryKey = notification.data.primaryKey;
+    var action = e.action;
+  
+    if (action === 'close') {
+      notification.close();
+    } else {
+      clients.openWindow('http://www.google.com');
+      notification.close();
+    }
+  });
+/**Manejo del evento de inserción en el trabajador de servicio */
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'This notification was generated from a push!',
+      icon: 'images/example.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {action: 'explore', title: 'Explore this new world',
+          icon: 'images/checkmark.png'},
+        {action: 'close', title: 'Close',
+          icon: 'images/xmark.png'},
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('Hello world!', options)
+    );
+  });
+  
+  
